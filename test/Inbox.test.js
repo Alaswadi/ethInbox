72248698b1c584f0ae3e6929964baf70af0f8d69
Fadi Al-Aswadi <fadi.alaswadi@gmail.com>
2018-03-06 18:48:03 +0300

    last commit2

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile')
  
  let accounts;
@@ -14,13 +15,26 @@ accounts = await web3.eth.getAccounts()
 inbox = await new web3.eth.Contract(JSON.parse(interface))
 .deploy({ data: bytecode, arguments: ['hey there']})
 .send ({ from: accounts[0], gas: '1000000'});
+
+ inbox.setProvider(provider);
 });
 
 describe('Inbox', () =>{
 it('deploys a contract', () => {
-console.log (inbox);
+assert.ok(inbox.options.address);
+});
+
+it('has a default message', async () =>{
+const message = await inbox.methods.message().call();
+assert.equal(message, 'hey there');
+});
+it('can change the message', async () => {
+await inbox.methods.setMessage('bye bye').send({ from: accounts[0] });
+const message = await inbox.methods.message().call();
+assert.equal(message, 'bye bye');
 });
 });
+
 
  /*class Car{
 
